4ad47524f0bd04738c0d328446dad73c6581803b
CoumbaBKANE <coumbakane41@gmail.com>
2024-04-08 15:46:48 +0000

    upload de classe surface polygon cercle ellipse

--- geoanalatyque/model/Polygone.java
+++ geoanalatyque/model/Polygone.java
@@ -1,4 +1,22 @@
 package model;
 
-public class Polygone extends GeoObject{
+/**
+ * La classe Polygone est une classe abstraite qui représente un polygone dans un espace bidimensionnel.
+ * Elle étend la classe abstraite Surface.
+ */
+public abstract class Polygone extends Surface {
+   
+    private int N;        // nombre de points du polygone, également le nombre de côtés
+    private Point[] a;    // tableau de points représentant les sommets du polygone
+
+    /**
+     * Calcule et retourne le périmètre du polygone.
+     * @return Le périmètre du polygone.
+     */
+    public double perimeter() {
+        double perimetre = 0.0;
+        for (int i = 0; i < N; i++)
+            perimetre = perimetre + a[i].distanceTo(a[i+1]);
+        return perimetre;
+    }
 }
--- geoanalatyque/model/Surface.java
+++ geoanalatyque/model/Surface.java
@@ -1,4 +1,21 @@
 package model;
 
-public class Surface extends GeoObject{
+/**
+ * La classe Surface est une classe abstraite qui représente un objet géométrique à deux dimensions.
+ * Elle étend la classe abstraite GeoObject.
+ */
+public abstract class Surface extends GeoObject {
+
+
+    /**
+     * Calcule et retourne l'aire de la surface.
+     * @return L'aire de la surface.
+     */
+    public abstract double calculAire();
+
+    /**
+     * Calcule et retourne le périmètre de la surface.
+     * @return Le périmètre de la surface.
+     */
+    public abstract double calculPerimetre();
 }
