e77c40825c6770ee97ae8d4827dac963fc3b9614
CallMeSama <diarratoulaye07@gmail.com>
2024-04-14 15:27:21 +0000

    polygone et triangle update

--- geoanalatyque/model/Polygone.java
+++ geoanalatyque/model/Polygone.java
@@ -16,12 +16,4 @@ public abstract class Polygone extends Surface {
      * Calcule et retourne le périmètre du polygone.
      * @return Le périmètre du polygone.
      */
-
-    public double calculPerimetre() {
-        double perimetre = 0.0;
-        for (int i = 0; i < sommets.length - 1; i++)
-            perimetre = perimetre + sommets[i].calculerDistance(sommets[i+1]);
-        perimetre = perimetre + sommets[sommets.length - 1].calculerDistance(sommets[0]); // Ajouter la distance entre le dernier et le premier point
-        return perimetre;
-    }
 }
--- geoanalatyque/model/Triangle.java
+++ geoanalatyque/model/Triangle.java
@@ -29,28 +29,27 @@ public abstract class Triangle extends Polygone {
      */
     public Triangle(Point s1, Point s2, Point s3) {
         super(new Point[]{s1, s2, s3});
-        calculDesCotes();
     }
 
     /**
      * Définit les longueurs des côtés du triangle en calculant la distance entre chaque paire de points.
      */
-    private void calculDesCotes() {
+    /*private void calculDesCotes() {
         coteA = sommets[0].calculerDistance(sommets[1]);
         coteB = sommets[1].calculerDistance(sommets[2]);
         coteC = sommets[2].calculerDistance(sommets[0]);
-    }
+    }*/
 
     /**
      * Méthode abstraite qui calcule l'aire du triangle connaissant la longueur de ses côté avec la methode de Heron.
      *
      * @return l'aire du triangle.
      */
-    public double calculAire(){
+    /* public double calculAire(){
         // Calculer le demi-périmètre
         double s = (coteA + coteB + coteC) / 2;
 
         // Calculer l'aire en utilisant la formule de Heron
         return Math.sqrt(s * (s - coteA) * (s - coteB) * (s - coteC));
-    };
+    }; */
 }
